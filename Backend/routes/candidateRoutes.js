7fd73f4a04299ae422f03bfe98414519ffadb065
ShaanAgarwal <111983758+ShaanAgarwal@users.noreply.github.com>
2023-08-27 20:59:01 +0530

    Fetching Candidate Details Feature Is Complete (#8)

--- Backend/routes/candidateRoutes.js
+++ Backend/routes/candidateRoutes.js
@@ -4,5 +4,6 @@ const candidateController = require('../controllers/candidateController');
 
 router.get('/check-password-change/:email', candidateController.checkForFirstLogin);
 router.post('/first-login-change-password', candidateController.changePasswordOnFirstLogin);
+router.get('/candidate-details', candidateController.getCandidateDetails);
 
 module.exports = router;

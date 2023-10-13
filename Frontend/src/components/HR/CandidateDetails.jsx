d1ef0b6b6ea001334e58c15d60e6cdf8c802be9f
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2023-10-13 22:24:01 +0530

    Removed mistake from previous commit

--- Frontend/src/components/HR/CandidateDetails.jsx
+++ Frontend/src/components/HR/CandidateDetails.jsx
@@ -95,9 +95,6 @@ const HRViewCandidate = () => {
           )}
         </div>
       ))}
-      {candidate.interviewClear && (
-        <button>Onboarding Process</button>
-      )}
     </div>
   );
 };

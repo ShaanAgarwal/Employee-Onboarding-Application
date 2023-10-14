f6a5f7db14cd3fe2d6fafa789518f1780002aa36
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2023-10-14 17:09:47 +0530

    Fixed Frontend Design For Head HR Viewing Applications

--- Frontend/src/components/HeadHR/HeadHRViewApplications.jsx
+++ Frontend/src/components/HeadHR/HeadHRViewApplications.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import "./HeadHRStyles/HeadHRViewApplicationsStyles.css";
 
 const HeadHRViewApplications = () => {
   const [candidates, setCandidates] = useState([]);
@@ -36,26 +37,32 @@ const HeadHRViewApplications = () => {
   };
 
   return (
-    <div>
-      <h2>Candidate List</h2>
-      <ul>
-        {candidates.map((candidate) => (
-          <li key={candidate._id}>
-            <p>Name: {candidate.name}</p>
-            <p>Email: {candidate.email}</p>
-            <p>Address: {candidate.address}</p>
-            <p>City: {candidate.city}</p>
-            <p>Pincode: {candidate.pincode}</p>
-            <p>Start Date: {new Date(candidate.start_date).toLocaleDateString()}</p>
-            <p>Job Role: {candidate.job_role}</p>
-            <p>Resume Path: <a href={candidate.resumePath} download>Download Resume</a></p>
-            <p>Photo: <img src={candidate.photoPath} alt="Candidate Photo" style={{ maxWidth: '200px' }} /></p>
-            <button onClick={() => handleAccept(candidate._id)}>Accept</button>
-            <button onClick={() => handleReject(candidate._id)}>Reject</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className='head-hr-view-applications-main-page'>
+        <h2 className='applications-header'>Applications</h2>
+        <div className='outer-padding'>
+          <ul>
+            {candidates.map((candidate) => (
+              <li key={candidate._id}>
+                <div className='candidate-photo'>
+                  <img src={candidate.photoPath} alt="Candidate Photo" />
+                </div>
+                <div className='candidate-info'>
+                  <p className='candidate-name'>{candidate.name}</p>
+                  <p className='candidate-email'>{candidate.email}</p>
+                  <p className='candidate-position'>Job Role: {candidate.job_role}</p>
+                  <p><a href={candidate.resumePath} download>Download Resume</a></p>
+                </div>
+                <div className='buttons-container'>
+                  <button className='approve' onClick={() => handleAccept(candidate._id)}>Approve</button>
+                  <button className='reject' onClick={() => handleReject(candidate._id)}>Reject</button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 

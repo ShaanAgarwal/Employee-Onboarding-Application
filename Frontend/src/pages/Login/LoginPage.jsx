94a8e6f50848dea6db522a238a63c7858a7a4314
Vaishnavi Borkar <vb.082cheenu@gmail.com>
2023-10-15 13:42:51 +0530

    login design

--- Frontend/src/pages/Login/LoginPage.jsx
+++ Frontend/src/pages/Login/LoginPage.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
+import "./LoginPage.css";
+import Logo from "./login-img.png";
+import Comlogo from "./Logo.png";
+import Key from "./key.svg";
+import Mail from "./envelop.svg";
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -27,21 +31,39 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
+    <div className="log-in">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="rectangle" />
+          <div className="group">
+            <div className="text-wrapper">LOG IN</div>
+            <div className="div">
+              <div className="overlap-group">
+                <div className="email-address">
+                  <img className="line" alt="Line" src="line-2.svg" />
+                  <div className="text-wrapper-2">Email Address</div>
+                </div>
+                <img className="mail" alt="Mail" src={Mail} />
+                <div className="password">
+                  <img className="key" alt="Mail" src={Key} />
+                  <div className="text-wrapper-2">Password</div>
+
+                </div>
+
+              </div>
+
+            </div>
+            <div className="create-account">
+              <div className="overlap-2">
+                <div className="rectangle-2" />
+                <div className="text-wrapper-3">LOG IN</div>
+              </div>
+            </div>
+          </div>
+          <img className="abstraction" alt="Abstraction" src={Logo} />
+          <img className="botanix" alt="Botanix" src={Comlogo} />
+        </div>
+      </div>
     </div>
   );
 };

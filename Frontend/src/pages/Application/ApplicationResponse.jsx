d14b84c71fb17e4b36e69bd2d0139f81fd69d5a7
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2023-10-16 15:50:59 +0530

    Thankyou Page After Candidate Applicaton

--- Frontend/src/pages/Application/ApplicationResponse.jsx
+++ Frontend/src/pages/Application/ApplicationResponse.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import "./ApplicationResponseStyles.css";
+import CompanyLogo from "./Empowerin-Logo.png";
+import ResponseCentralLogo from "./Application-Page-Response-Central-Logo.png";
+import globeLogo from "./globe.png";
+import facebookLogo from "./facebook-logo.png";
+import gmailLogo from "./gmail-logo.png";
+import instagramLogo from "./instagram-logo.png";
 
 const ApplicationResponse = () => {
     return (
         <>
-        <div>Thankyou for applying. We will get back to you soon.</div>
-        <Link to="/" className='button apply-button'>Go Back To HomePage</Link>
+            <div className='main-application-response-page'>
+                <div className='middle-portion'>
+                    <div className='good-luck'>
+                        <p>Good luck with the interviews</p>
+                    </div>
+                    <div className='contains-homepage-button'>
+                        <Link to="/" className=''><p>BACK TO HOMEPAGE</p></Link>
+                    </div>
+                </div>
+                <div className="central-logo">
+                    <img src={ResponseCentralLogo} alt="Response Central Logo" />
+                    <h1>Thanks for applying at our portal!</h1>
+                </div>
+                <div className='top-portion'>
+                    <div>
+                        <img className='company-logo' src={CompanyLogo} alt="Company Logo" />
+                    </div>
+                </div>
+                <div className='bottom-portion'>
+                    <img className="globe" src={globeLogo} alt="" />
+                    <p>INDIA</p>
+                    <img className="facebook-logo" src={facebookLogo} alt="" />
+                    <img className="gmail-logo" src={gmailLogo} alt="" />
+                    <img className="instagram-logo" src={instagramLogo} alt="" />
+                </div>
+            </div>
         </>
     );
 };

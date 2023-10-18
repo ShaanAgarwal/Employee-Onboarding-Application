8a1b50b7f7ec7700cb4f70ce34902cf357991e4f
ShaanAgarwal <111983758+ShaanAgarwal@users.noreply.github.com>
2023-10-18 12:09:20 +0530

    hr view all candidates (#18)

--- Frontend/src/components/HR/HRComponents.jsx
+++ Frontend/src/components/HR/HRComponents.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import "./Styles/HRComponentStyles.css";
+import companyLogo from "./company-logo.png";
+import profileSideImage from "./profile-side-image.png";
 
 const HRComponents = () => {
     return (
-        <div>HRComponents</div>
+        <>
+            <div className='hr-component'>
+                <img className='company-logo' src={companyLogo} alt="" />
+                <img className='profile-side-image' src={profileSideImage} alt="" />
+            </div>
+        </>
     );
 };
 

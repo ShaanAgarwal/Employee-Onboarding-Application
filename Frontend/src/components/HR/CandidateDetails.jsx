a17e38702d5cf96f48a28e13a92dcc8d37acd5ba
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2023-10-30 18:53:09 +0530

    second commit

--- Frontend/src/components/HR/CandidateDetails.jsx
+++ Frontend/src/components/HR/CandidateDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams, Link } from "react-router-dom";
 import axios from "axios";
+import "./Styles/CandidateDetailsStyles.css";
 
 const HRViewCandidate = () => {
   const { candidateId } = useParams();
@@ -66,7 +67,7 @@ const HRViewCandidate = () => {
   const handleRejectRound = async (roundId) => {
     try {
       const token = localStorage.getItem('token');
-      await axios.put(`http://localhost:8080/api/hr/round/${roundId}/reject`,{
+      await axios.put(`http://localhost:8080/api/hr/round/${roundId}/reject`, {
         headers: {
           Authorization: token,
         }
@@ -82,44 +83,46 @@ const HRViewCandidate = () => {
   }
 
   return (
-    <div>
-      <h2>Candidate Details</h2>
-      <p>Name: {candidate.name}</p>
-      <p>Email: {candidate.email}</p>
-      <p>Number Of Rounds: {candidate.rounds}</p>
+    <div className="hr-candidate-main">
+      <div className="main-box-inside">
+        <h2>Candidate Details</h2>
+        <p>Name: {candidate.name}</p>
+        <p>Email: {candidate.email}</p>
+        <p>Number Of Rounds: {candidate.rounds}</p>
 
-      <h3>Interview Rounds</h3>
-      {candidate.interviewRounds.map((round, index) => (
-        <div key={round._id}>
-          <p>Round {index + 1} </p>
-          <p>Name: {round.name} </p>
-          <p>Details: {round.details}</p>
-          {index === candidate.currentRound - 1 && (
-            <>
-              <form>
-                <input
-                  type="text"
-                  value={nameInput}
-                  onChange={e => setNameInput(e.target.value)}
-                />
-                <input
-                  type="text"
-                  value={detailsInput}
-                  onChange={e => setDetailsInput(e.target.value)}
-                />
-                <button type="button" onClick={() => handleUpdateRound(round._id)}>Update</button>
-              </form>
-              {round.attempted && (
-                <>
-                  <button onClick={() => handleAcceptRound(round._id)}>Accept</button>
-                  <button onClick={() => handleRejectRound(round._id)}>Reject</button>
-                </>
-              )}
-            </>
-          )}
-        </div>
-      ))}
-      {candidate.interviewClear == true && (<Link to={`/dashboard/HRViewCandidates/${candidate._id}/PersonalDetails`}>Onboarding</Link>)}
+        <h3>Interview Rounds</h3>
+        {candidate.interviewRounds.map((round, index) => (
+          <div key={round._id}>
+            <p>Round {index + 1} </p>
+            <p>Name: {round.name} </p>
+            <p>Details: {round.details}</p>
+            {index === candidate.currentRound - 1 && (
+              <>
+                <form>
+                  <input
+                    type="text"
+                    value={nameInput}
+                    onChange={e => setNameInput(e.target.value)}
+                  />
+                  <input
+                    type="text"
+                    value={detailsInput}
+                    onChange={e => setDetailsInput(e.target.value)}
+                  />
+                  <button type="button" onClick={() => handleUpdateRound(round._id)}>Update</button>
+                </form>
+                {round.attempted && (
+                  <>
+                    <button onClick={() => handleAcceptRound(round._id)}>Accept</button>
+                    <button onClick={() => handleRejectRound(round._id)}>Reject</button>
+                  </>
+                )}
+              </>
+            )}
+          </div>
+        ))}
+        {candidate.interviewClear == true && (<Link to={`/dashboard/HRViewCandidates/${candidate._id}/PersonalDetails`}>Onboarding</Link>)}
+      </div>
     </div>
   );
 };

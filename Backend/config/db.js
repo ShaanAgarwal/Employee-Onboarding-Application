ad031550b2115a45a350861378e659199d7b49b5
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2023-10-30 21:31:33 +0530

    Removed uncessary code from db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,11 +3,11 @@ const colors = require("colors");
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL);
         console.log(`Connected To MongoDB Database ${mongoose.connection.host}`.bgMagenta.white);
     } catch (error) {
-        console.log(`MongoDB Error ${error}`.bgRed.white);
-    };
+        console.log(`MongoDB Error: ${error}`.bgRed.white);
+    }
 };
 
 module.exports = { connectDB };

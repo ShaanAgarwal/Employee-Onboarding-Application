acb27bd1699b55ded14d5b823d42e49041986114
ShaanAgarwal <111983758+ShaanAgarwal@users.noreply.github.com>
2023-10-31 22:42:08 +0530

    offer letter page words (#41)

--- Frontend/src/pages/Candidate/Candidate-Onboarding/OfferLetter.jsx
+++ Frontend/src/pages/Candidate/Candidate-Onboarding/OfferLetter.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './OfferLetterStyles.css'; // Import the CSS file for styling
 
 const OfferLetter = () => {
   return (
-    <div>OfferLetter</div>
-  )
-}
+    <div className="offer-letter-container">
+      <div className="offer-letter-content">
+        <p className="offer-text">THE OFFER LETTER WILL BE MAILED TO YOU SHORTLY</p>
+      </div>
+    </div>
+  );
+};
 
-export default OfferLetter
+export default OfferLetter;

e53c6aebc41a041595bcce08ea6da305c3138857
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2023-10-31 18:46:44 +0530

    resolved jwt error

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -11,6 +11,7 @@ const User = require("../models/userSchema");
 const bcrypt = require("bcrypt");
 const KEYFILEPATH = path.join(__dirname, "cred.json");
 const SCOPES = ["https://www.googleapis.com/auth/drive"];
+const jwt = require('jsonwebtoken');
 
 const auth = new google.auth.GoogleAuth({
   keyFile: KEYFILEPATH,
@@ -74,26 +75,17 @@ const login = async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(401).json({ message: "Invalid credentials" });
-    }
+      return res.status(401).json({ message: 'Invalid credentials' });
+    };
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
-      return res.status(401).json({ message: "Invalid credentials" });
-    }
-    const token = jwt.sign({ userId: user._id }, "sjdkfhajskdfhaksjdfh", {
-      expiresIn: "1h",
-    });
-    return res.json({
-      token,
-      userId: user._id,
-      email: user.email,
-      role: user.role,
-    });
+      return res.status(401).json({ message: 'Invalid credentials' });
+    };
+    const token = jwt.sign({ userId: user._id }, 'sjdkfhajskdfhaksjdfh', { expiresIn: '1h' });
+    return res.status(200).json({ token, userId: user._id, email: user.email, role: user.role });
   } catch (error) {
-    return res
-      .status(500)
-      .json({ message: "An error occurred", error: error.message });
-  }
+    return res.status(500).json({ message: 'An error occurred', error: error.message });
+  };
 };
 
 module.exports = { register, login };

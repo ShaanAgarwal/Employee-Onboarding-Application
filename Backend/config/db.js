061f21201df68f11eb694c488484c1fc2f83d93f
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2023-11-07 20:11:13 +0530

    added credentials to project

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect("mongodb+srv://shaanagarwalofficial:A4Tdy4h7wiADw3EX@employee-onboarding-dat.pfiw26x.mongodb.net/");
         console.log(`Connected To MongoDB Database ${mongoose.connection.host}`.bgMagenta.white);
     } catch (error) {
         console.log(`MongoDB Error: ${error}`.bgRed.white);

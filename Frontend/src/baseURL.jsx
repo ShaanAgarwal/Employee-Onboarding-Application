e503debb14e9c6074de25867497136b051321333
ShaanAgarwal <shaanagarwalofficial@gmail.com>
2023-12-25 17:51:02 +0530

    one small change

--- Frontend/src/baseURL.jsx
+++ Frontend/src/baseURL.jsx
@@ -1,2 +1,2 @@
-const backendURL = "https://employee-onboarding-application-backend.vercel.app/";
+const backendURL = "https://employee-onboarding-application-backend.vercel.app";
 export default backendURL;

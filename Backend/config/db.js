cd9bfafd0e9cc2a744572667cd4baef01371120a
ShaanAgarwal <shaanagarwalofficial@gmail.com>
2023-12-25 17:36:31 +0530

    Added Database Credentials

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://shaanagarwal1942003:Qxi6rj8GBKQ2OcgT@cluster0.pa3lrct.mongodb.net/");
+        await mongoose.connect("mongodb+srv://shaanagarwal1942003:zZCF8XElX5ckVuGW@cluster0.rp8jg5h.mongodb.net/");
         console.log(`Connected To MongoDB Database ${mongoose.connection.host}`.bgMagenta.white);
     } catch (error) {
         console.log(`MongoDB Error: ${error}`.bgRed.white);

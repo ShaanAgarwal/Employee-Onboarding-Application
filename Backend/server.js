f6ef74415113f33d413664e9b3a0a065f9fd6a02
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2024-06-25 11:35:45 +0530

    Modified Some Configurations

--- Backend/server.js
+++ Backend/server.js
@@ -3,14 +3,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const morgan = require("morgan");
-const bodyParser = require('body-parser');
-const formRoutes = require('./routes/formRoutes');
-const authRoutes = require('./routes/authRoutes');
-const adminRoutes = require('./routes/adminRoutes');
-const hrRoutes = require('./routes/hrRoutes');
-const candidateRoutes = require('./routes/candidateRoutes');
-const onboardingRoutes = require('./routes/onboardingRoutes');
-const chatRoutes = require('./routes/chatRoutes');
+const bodyParser = require("body-parser");
+const formRoutes = require("./routes/formRoutes");
+const authRoutes = require("./routes/authRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const hrRoutes = require("./routes/hrRoutes");
+const candidateRoutes = require("./routes/candidateRoutes");
+const onboardingRoutes = require("./routes/onboardingRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 
 // Importing Mongoose
 const { connectDB } = require("./config/db");
@@ -31,19 +31,21 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/api/form', formRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/admin', adminRoutes);
-app.use('/api/hr', hrRoutes);
-app.use('/api/candidate', candidateRoutes);
-app.use('/api/onboarding', onboardingRoutes);
-app.use('/api/chat', chatRoutes);
+app.use("/api/form", formRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/hr", hrRoutes);
+app.use("/api/candidate", candidateRoutes);
+app.use("/api/onboarding", onboardingRoutes);
+app.use("/api/chat", chatRoutes);
 
 // Port
-const PORT = 8080;
+const PORT = 8081;
 const MODE = "development";
 
 // Listen
 app.listen(PORT, (req, res) => {
-    console.log(`Node Server Running In ${MODE} Mode on port no ${PORT}`.bgCyan.white);
+  console.log(
+    `Node Server Running In ${MODE} Mode on port no ${PORT}`.bgCyan.white
+  );
 });
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Employee-Onboarding-Application
-CITL Lab Experiment 1 - Semester 5
+
+## To Run The Frontend
+(By Default The Frontend Works On Port 5173. To Change The Port Make Changes In The Docker/docker-compose, vite.config.js file and the terminal commands as well.)
+
+(Using These Commands The Application Has To Be Built Again With Each and every change)
+docker build -t empowerin-frontend .
+docker run -p 5173:5173 empowerin-frontend
+
+(Using These Commands, Hot Reloading Is Enabled)
+docker-compose up
+
+## To Run The Backend
+(By Default The Backend Works On Port 8081. To Change The Port Make Changes In The Docker/docker-compose, server.js file and the terminal commands as well.)
+
+(Using These Commands The Application Has To Be Built Again With Each and every change)
+docker build -t empowerin-backend .
+docker run -p 8081:8081 -e PORT=8081 empowerin-backend
+
+(Using These Commands, Hot Reloading Is Enabled)
+docker-compose up
+
+## Changing Backend URL
+(Put The Backend URL in the /baseURL.jsx file in the frontend directory.)

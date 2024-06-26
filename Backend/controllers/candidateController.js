9df49ecf7f2d54ff6734fb5e0b8a080e752457f9
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2024-06-27 00:38:33 +0530

    one more small change

--- Backend/controllers/candidateController.js
+++ Backend/controllers/candidateController.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const router = express.Router();
 const User = require('../models/userSchema');
 
 const checkForFirstLogin = async (req, res) => {
